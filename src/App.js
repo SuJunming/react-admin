cd5d14b30bb42598325ba235ceb9821c5e54fc91
sujunming <827606400@qq.com>
2017-08-31 19:23:37 +0800

    new buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Nav from "./router/Nav";
 import RouterUrl from "./router/RouterUrl";
 import { Layout, Menu, Breadcrumb, Icon, Button } from "antd";
@@ -19,19 +18,19 @@ class App extends Component {
     return (
       <div className="App">
         <Layout style={{ height: "100%" }}>
-        <div className="logo" />
-          <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className='logo'/>
             <Nav />
           </Sider>
           <Layout>
             <Header
-              style={{ background: "#fff", padding: 0, paddingLeft: "20px" }}
+              style={{ background: "#424242 !important", padding: 0, paddingLeft: "20px" }}
             >
               <Icon
                 className="trigger"
                 type={this.state.collapsed ? "menu-unfold" : "menu-fold"}
                 onClick={this.toggle}
-                style={{ fontSize: 20 }}
+                style={{ fontSize: 20,color:'#fff' }}
               />
             </Header>
               <RouterUrl/>
--- src/components/UI/buttons.js
+++ src/components/UI/buttons.js
@@ -7,45 +7,12 @@ class Buttons extends Component {
     return (
       <div>
         <Row>
-        <Col span={10}>
+        <Col span={12}>
           <Content
             style={{
-              margin: "24px 5px",
-              background: "#fff",
-              padding: 50,
-            }}
-          >
-            <Button type="primary">Primary</Button>
-            <Button>Default</Button>
-            <Button type="dashed">Dashed</Button>
-            <Button type="danger">Danger</Button>
-          </Content>
-          </Col>
-          <Col span={14}>
-          <Content
-            style={{
-              margin: "24px 5px",
-              padding: 50,
-              background: "#fff",
-            }}
-          >
-          <Button type="primary" shape="circle" icon="search" />
-          <Button type="primary" icon="search">Search</Button>
-          <Button shape="circle" icon="search" />
-          <Button icon="search">Search</Button>
-          <br />
-          <Button shape="circle" icon="search" />
-          <Button icon="search">Search</Button>
-          <Button type="dashed" shape="circle" icon="search" />
-          <Button type="dashed" icon="search">Search</Button>
-          </Content>
-          </Col>
-          </Row>
-          <Content
-            style={{
-              margin: "24px 16px",
+              margin: "10px 5px",
+              background: "rgba(255, 255, 255,0.6)",
               padding: 30,
-              background: "#fff",
             }}
           >
           <h4>Basic</h4>
@@ -64,7 +31,7 @@ class Buttons extends Component {
             <Button>M</Button>
             <Button type="dashed">R</Button>
           </ButtonGroup>
-      
+    
           <h4>With Icon</h4>
           <ButtonGroup>
             <Button type="primary">
@@ -79,18 +46,89 @@ class Buttons extends Component {
             <Button type="primary" icon="cloud-download" />
           </ButtonGroup>
           </Content>
+          </Col>
+          <Col span={12}>
+          <Content
+            style={{
+              margin: "10px 5px",
+              padding: 30,
+              background: "rgba(255, 255, 255,0.6)",
+            }}
+          >
+          <Button type="primary" shape="circle" icon="search" />
+          <Button type="primary" icon="search">Search</Button>
+          <Button shape="circle" icon="search" />
+          <Button icon="search">Search</Button>
+          <br />
+          <Button shape="circle" icon="search" />
+          <Button icon="search">Search</Button>
+          <Button type="dashed" shape="circle" icon="search" />
+          <Button type="dashed" icon="search">Search</Button>
+          </Content>
+          </Col>
           <style>
           {
               `
               .ant-btn{
-                  margin:10px
+                  margin:10px;
               }
               .ant-layout-content{
-                    min-height:200px
+                    text-align:center;
               }
               `
           }
           </style>
+          </Row>
+          <Row>
+          <Col span={12}>
+          <Content
+            style={{
+              margin: "10px 16px",
+              padding: 30,
+              background: "rgba(255, 255, 255,0.6)",
+            }}
+          >
+          <Button type="primary">Primary</Button>
+          <Button>Default</Button>
+          <br/>
+          <Button type="dashed">Dashed</Button>
+          <Button type="danger">Danger</Button>
+          </Content>
+          </Col>
+          <Col span={12}>
+          <Content
+            style={{
+              margin: "10px 5px",
+              background: "rgb(190, 200, 200)",
+              padding: 30,
+            }}
+          >
+          <Button type="primary" ghost>Primary</Button>
+          <Button ghost>Default</Button>
+          <br/>
+          <Button type="dashed" ghost>Dashed</Button>
+          <Button type="danger" ghost>danger</Button>
+          </Content>
+          </Col>
+          <Col span={24}>
+          <Content
+            style={{
+              margin: "10px 5px",
+              background: "rgba(255, 255, 255,0.6)",
+              padding: 30,
+            }}
+          >
+          <Button type="primary">Primary</Button>
+          <Button type="primary" disabled>Primary(disabled)</Button>
+          <br />
+          <Button>Default</Button>
+          <Button disabled>Default(disabled)</Button>
+          <br />
+          <Button>Ghost</Button>
+          <Button disabled>Ghost(disabled)</Button>
+          </Content>
+          </Col>
+          </Row>
       </div>
     );
   }
