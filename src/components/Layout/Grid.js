4e634ce6f8260d47714cbde2435fa4b7f95b49be
fusheng <fusheng@netposa.com>
2017-12-28 15:03:15 +0800

    fix

--- src/components/Layout/Grid.js
+++ src/components/Layout/Grid.js
@@ -8,21 +8,44 @@ export const Grid = (props) => {
             <Content className={styles.layoutGridContent}>
                 <h2>Basic use</h2>
                 <div>
-                    <Row span={24}>col-24</Row>
                     <Row>
-                        <Col span={12}>col-12</Col>
-                        <Col span={12}>col-12</Col>
+                        <Col span={24} className={styles.layoutGridItem}>col-24</Col>
                     </Row>
                     <Row>
-                        <Col span={8}>col-8</Col>
-                        <Col span={8}>col-8</Col>
-                        <Col span={8}>col-8</Col>
+                        <Col span={12} className={styles.layoutGridItem}>col-12</Col>
+                        <Col span={12} className={styles.layoutGridItem}>col-12</Col>
                     </Row>
                     <Row>
-                        <Col span={6}>col-6</Col>
-                        <Col span={6}>col-6</Col>
-                        <Col span={6}>col-6</Col>
-                        <Col span={6}>col-6</Col>
+                        <Col span={8} className={styles.layoutGridItem}>col-8</Col>
+                        <Col span={8} className={styles.layoutGridItem}>col-8</Col>
+                        <Col span={8} className={styles.layoutGridItem}>col-8</Col>
+                    </Row>
+                    <Row>
+                        <Col span={6} className={styles.layoutGridItem}>col-6</Col>
+                        <Col span={6} className={styles.layoutGridItem}>col-6</Col>
+                        <Col span={6} className={styles.layoutGridItem}>col-6</Col>
+                        <Col span={6} className={styles.layoutGridItem}>col-6</Col>
+                    </Row>
+                </div>
+                <h2>Advance use</h2>
+                <div>
+                    <Row>
+                        <Col span={24} className={styles.layoutGridItem}>col-24</Col>
+                    </Row>
+                    <Row>
+                        <Col span={12} className={styles.layoutGridItem}>col-12</Col>
+                        <Col span={12} className={styles.layoutGridItem}>col-12</Col>
+                    </Row>
+                    <Row>
+                        <Col span={8} className={styles.layoutGridItem}>col-8</Col>
+                        <Col span={8} className={styles.layoutGridItem}>col-8</Col>
+                        <Col span={8} className={styles.layoutGridItem}>col-8</Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col span={6} className={styles.layoutGridItem}>col-6</Col>
+                        <Col span={6} className={styles.layoutGridItem}>col-6</Col>
+                        <Col span={6} className={styles.layoutGridItem}>col-6</Col>
+                        <Col span={6} className={styles.layoutGridItem}>col-6</Col>
                     </Row>
                 </div>
             </Content>
